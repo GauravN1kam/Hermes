fe5794820a9e44ce2e6da438492c40d4e7abf2e9
Gaurav Nikam <gauravpn.a2@gmail.com>
2026-02-17 22:43:15 +0530

    Completed a method

--- src/json_parser.cpp
+++ src/json_parser.cpp
@@ -1,3 +1,13 @@
 #include "../include/json_parser.hpp"
+#include <map>
 
-std::string parse_json(const char *json) {}
+using JsonObject = std::map<std::string, JsonValue>;
+using JsonArray = std::vector<JsonValue>;
+
+struct JsonValue {
+  enum class Type { Null, Boolean, Number, String, Array, Object };
+
+  Type type;
+  std::variant<std::nullptr_t, bool, double, std::string, JsonArray, JsonObject>
+      value;
+};
